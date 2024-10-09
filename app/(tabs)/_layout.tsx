4c31beb214e2819f516cb544df057345e495063a
denis <denis961111@gmail.com>
2024-10-08 18:17:11 -0700

    add map view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,11 +24,11 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="map"
         options={{
-          title: 'Explore',
+          title: 'Map',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'map' : 'map-outline'} color={color} />
           ),
         }}
       />
